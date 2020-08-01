78ca60c83521eef6cd11f86e1cad577e69cc1c71
Parth Khunteta <parthkhunteta96@gmail.com>
2020-08-01 22:24:45 +0530

    third

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import db from './firebase';
 import SendIcon from '@material-ui/icons/Send';
 import { IconButton } from '@material-ui/core';
 
+
+
 function App() {
 
   const [input, setInput] = useState('');
   const [messages, setMessages] = useState([]);
   const [username, setUsername] = useState('');
 
-
+  console.log(messages);
   useEffect(() => {
     db.collection('messages').orderBy('timeStamp', 'desc').onSnapshot(snapshot => {
       setMessages(snapshot.docs.map(doc => ({ id: doc.id, message: doc.data() })
@@ -38,12 +40,12 @@ function App() {
   return (
     <div className="App">
       <img alt="Messenger_Image" src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100" />
-      <h1>Happy Chatting</h1>
-      <h2>Welcome {username}</h2>
+      <h1><strong>Messenger App</strong></h1>
+      <h3 className="app__username"><em>Welcome {username}</em></h3>
 
       <form className="app__form">
         <FormControl className="app__formControl">
-          <Input className="app__input" placeholder='Enter a message ...' value={input} onChange={(event) => setInput(event.target.value)} />
+          <Input className="app__input" placeholder='Type a message and press Enter...' value={input} onChange={(event) => setInput(event.target.value)} />
           <IconButton
             className="app__iconButton"
             disabled={!input}
@@ -53,7 +55,6 @@ function App() {
             onClick={sendMessage}>
             <SendIcon />
           </IconButton>
-
         </FormControl>
       </form>
 
@@ -62,8 +63,10 @@ function App() {
         {
           messages.map(({ id, message }) =>
             <Message key={id} username={username} message={message} />)
+
         }
       </FlipMove>
+
     </div>
   );
 }
--- src/Message.js
+++ src/Message.js
@@ -1,18 +1,26 @@
 import React, { forwardRef } from 'react'
+import moment from 'moment';
 import { Card, CardContent, Typography } from '@material-ui/core';
 import './Message.css';
 
 const Message = forwardRef(({ message, username }, ref) => {
     const isUser = username === message.username;
+    let newDate = null;
+    if (!isUser) {
+        newDate = moment(new Date(message.timeStamp.seconds * 1000)).format('DD/MM/YYYY hh:MM');
+    }
+
     return (
         <div ref={ref} className={`message ${isUser && 'message__user'}`}>
+            <h5 className="message__input">{!isUser && `${newDate || 'Unknown '}`}</h5>
+            <h5 className="message__input"> {!isUser && `${message.username || 'Unknown User'}`}</h5>
             <Card className={isUser ? "message__useCard" : "message__guestCard"}>
                 <CardContent>
                     <Typography
-                        color="white"
+                        color="inherit"
                         variant="h5"
                         component="h2">
-                        {!isUser && `${message.username || 'Unknown User'} : `} {message.message}
+                        {message.message}
                     </Typography>
                 </CardContent>
             </Card>
